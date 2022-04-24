5e8c3b24e124d4bbb6922716b38599ba6c26e0f5
Adrian Alberto <adrianalberto1986@gmail.com>
2022-04-24 13:59:31 -0500

    commit tree da5ad41be1c5100ffcf3a17e1b74c7c54f50b30f

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+const num1 = 31
+const num2  = 2
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1
+
+const num3 = 25
+const num4 = 7
+let mod = num3 % num4
+
+const max = Math.max(5, 10, 15, 20)
